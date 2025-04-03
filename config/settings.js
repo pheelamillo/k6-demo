7901be1fb51c8835224c9f1f1e65c4de113cee7b
Millo Ghajiga <pheelamillo@users.noreply.github.com>
2025-04-03 14:53:57 -0400

    Update settings.js

--- config/settings.js
+++ config/settings.js
@@ -1,1 +1,30 @@
 
+
+
+const EnvConfig = {
+  dev: {
+    BASE_URL: 'http://fdc-dev.com', 
+    MY_FLAG: true
+  },
+  qa: {
+    BASE_URL: 'http://fdc-qa.com',
+    MY_FLAG: true
+  },
+  stage: {
+    BASE_URL: 'http://fc-stage.com',
+    MY_FLAG: false
+  },
+  prod: {
+    BASE_URL: 'https://furniture.com',
+    MY_FLAG: false
+  }
+};
+
+const Config = EnvConfig[__ENV.ENVIRONMENT] || EnvConfig['dev'];
+const BASE_URL = Config.BASE_URL;
+
+
+
+//To run on enironments:
+// k6 run -e ENVIRONMENT=prod -e WORKLOAD=stress test.js
+// k6 run -e ENVIRONMENT=qa -e WORKLOAD=average new_test.js
